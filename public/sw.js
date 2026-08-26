97a112e8e163271da5994a9b97c5f02774eb7122
Rishabh kumar <therishx@gmail.com>
2026-08-26 22:31:17 +0545

    icon logo updated

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,18 @@
-const CACHE = 'el-roi-tunes-v1';
+const CACHE = 'el-roi-tunes-v2';
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(['/','/index.html','/manifest.webmanifest'])));
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll([
+    '/',
+    '/index.html',
+    '/manifest.webmanifest',
+    '/hebrew-lyre-logo.png?v=2',
+  ])));
   self.skipWaiting();
 });
-self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
+self.addEventListener('activate', (event) => event.waitUntil(
+  caches.keys().then((keys) => Promise.all(
+    keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)),
+  )).then(() => self.clients.claim()),
+));
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
   event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
